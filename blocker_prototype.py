8f9781f9758caea5a01496a1a972f5a036954d5b
Timmy <timmy@Timmys-Mac-mini.local>
2025-07-29 02:43:04 +0000

    start blocker

---
+++ blocker_prototype.py
@@ -0,0 +1,14 @@
+"""
+The prototype for the blocker
+
+The information for this comes from Todoist, so I must set that up first!
+
+Options:
+    - DNS Filtering
+    - Proxy Server
+    - Firewall Setup
+"""
+
+
+# I will start with the DNS Filtering
+# Which means I will start by grokking how DNS works
